8d897e83e8cf32e93c43cb11d74e4ddbf58a78bf
jRimbault <jacques.rimbault@gmail.com>
2020-03-23 16:35:16 +0100

    handle state in one place

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,31 @@
 import { createNode } from 'dom'
 
+type StateManager = ReturnType<typeof stateManager>
+
 function main() {
   const addButton = document.getElementById('add-player') as HTMLButtonElement | null
   const resetButton = document.getElementById('reset') as HTMLButtonElement | null
   const saveButton = document.getElementById('save') as HTMLButtonElement | null
   const playerNameInput = document.getElementById('player-name') as HTMLInputElement | null
   const playerListElement = document.getElementById('player-list') as HTMLUListElement | null
-  assert(addButton)
-  assert(resetButton)
-  assert(saveButton)
-  assert(playerNameInput)
-  assert(playerListElement)
-  const scores = getInitialState()
-  initPlayingBoard(scores, playerListElement)
+  assert(addButton !== null)
+  assert(resetButton !== null)
+  assert(saveButton !== null)
+  assert(playerNameInput !== null)
+  assert(playerListElement !== null)
+  const state: StateManager = stateManager()
+  const scores = state.get()
+  playerListElement.append(...initialPlayingBoard(scores))
   addButton.addEventListener('click', addPlayer(scores, playerListElement, playerNameInput))
   resetButton.addEventListener('click', () => {
-    scores.clear()
     Array.from(playerListElement.children).forEach(c => c.remove())
-    localStorage.removeItem('scores')
+    state.clear(scores)
   })
-  saveButton.addEventListener('click', () => {
-    const json: Record<string, number> = {}
-    for (const [playerName, playerScore] of scores.entries()) {
-      json[playerName] = playerScore
-    }
-    localStorage.setItem('scores', JSON.stringify(json))
-  })
+  saveButton.addEventListener('click', () => state.set(scores))
 }
 
-function getInitialState() {
-  const json: Record<string, number> = JSON.parse(localStorage.getItem('scores') || '{}')
-  const scores = new Map<string, number>()
-  for (const [playerName, playerScore] of Object.entries(json)) {
-    scores.set(playerName, playerScore)
-  }
-  return scores
-}
-
-function initPlayingBoard(scores: Map<string, number>, list: HTMLUListElement) {
-  for (const playerName of scores.keys()) {
-    list.append(makePlayerListItem(playerName, scores))
-  }
+function initialPlayingBoard(scores: Map<string, number>) {
+  return Array.from(scores.keys(), playerName => makePlayerListItem(playerName, scores))
 }
 
 function addPlayer(scores: Map<string, number>, list: HTMLUListElement, input: HTMLInputElement) {
@@ -93,6 +78,32 @@ function assert(condition: unknown, msg?: string): asserts condition {
   if (!condition) {
     throw new Error(msg)
   }
+}
+
+type PartialStorage = Pick<Storage, 'getItem' | 'setItem' | 'removeItem'>
+function stateManager(storage: PartialStorage = localStorage) {
+  const storageKey = 'scores'
+  return {
+    get: (): Map<string, number> => {
+      const json: Record<string, number> = JSON.parse(storage.getItem(storageKey) || '{}')
+      const scores = new Map<string, number>()
+      for (const [playerName, playerScore] of Object.entries(json)) {
+        scores.set(playerName, playerScore)
+      }
+      return scores
+    },
+    set: (scores: Map<string, number>) => {
+      const json: Record<string, number> = {}
+      for (const [playerName, playerScore] of scores.entries()) {
+        json[playerName] = playerScore
+      }
+      storage.setItem(storageKey, JSON.stringify(json))
+    },
+    clear: (scores: Map<string, number>) => {
+      scores.clear()
+      storage.removeItem(storageKey)
+    },
+  } as const
 }
 
 window.onload = main
